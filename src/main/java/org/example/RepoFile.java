f480ff362447c706289e6749b8f13164a78bb0ac
ns2007fp-stack <ns2007fp@gmail.com>
2025-11-24 10:03:23 +0100

    Se extrae las lineas y sus casos a metodos

--- src/main/java/org/example/RepoFile.java
+++ src/main/java/org/example/RepoFile.java
@@ -11,56 +11,86 @@ public class RepoFile {
         boolean salir = false;
 
         while (!salir) {
-            System.out.println(Constantes.AGENDA);
-            System.out.println(Constantes.LISTAR);
-            System.out.println(Constantes.BUSCAR_POR_DÍA);
-            System.out.println(Constantes.BUSCAR_POR_NOMBRE);
-            System.out.println(Constantes.AÑADIR);
-            System.out.println(Constantes.ELIMINAR);
-            System.out.println(Constantes.SALIR);
+            menu();
 
             String opcion = sc.nextLine();
 
             switch (opcion) {
                 case "1":
-                    for (cumpleAgenda c : repo.getLista()) {
-                        System.out.println(c.getNombre());
-                    }
+                    agenda(repo);
                     break;
 
                 case "2":
-                    System.out.print(Constantes.DÍA + " ");
-                    int dia = Integer.parseInt(sc.nextLine());
-                    for (cumpleAgenda c : repo.buscarPorDia(dia)) {
-                        System.out.println(c.getNombre());
-                    }
+                    dias(sc, repo);
                     break;
 
                 case "3":
-                    System.out.print(Constantes.NOMBRE + " ");
-                    cumpleAgenda c = repo.buscarPorNombre(sc.nextLine());
-                    if (c != null) System.out.println(c.getNombre());
+                    dias(repo, sc);
                     break;
 
                 case "4":
-                    System.out.print(Constantes.NOMBRE + " ");
-                    String nombre = sc.nextLine();
-                    System.out.print(Constantes.DÍA + " ");
-                    int d = Integer.parseInt(sc.nextLine());
-                    System.out.print("Mes: ");
-                    int m = Integer.parseInt(sc.nextLine());
-                    repo.addCumple(new cumpleAgenda(nombre, d, m));
+                    cumpleaños(sc, repo);
                     break;
 
                 case "5":
-                    System.out.print(Constantes.ELIMINAR_NOMBRE);
-                    repo.eliminarPorNombre(sc.nextLine());
+                    eliminarnombre(repo, sc);
                     break;
 
                 case "6":
-                    salir = true;
+                    salir = isSalir();
                     break;
             }
         }
+    }
+
+    private static boolean isSalir() {
+        boolean salir;
+        salir = true;
+        return salir;
+    }
+
+    private static void eliminarnombre(Main repo, Scanner sc) {
+        System.out.print(Constantes.ELIMINAR_NOMBRE);
+        repo.eliminarPorNombre(sc.nextLine());
+    }
+
+    private static void cumpleaños(Scanner sc, Main repo) {
+        System.out.print(Constantes.NOMBRE + " ");
+        String nombre = sc.nextLine();
+        System.out.print(Constantes.DÍA + " ");
+        int d = Integer.parseInt(sc.nextLine());
+        System.out.print("Mes: ");
+        int m = Integer.parseInt(sc.nextLine());
+        repo.addCumple(new cumpleAgenda(nombre, d, m));
+    }
+
+    private static void dias(Main repo, Scanner sc) {
+        System.out.print(Constantes.NOMBRE + " ");
+        cumpleAgenda c = repo.buscarPorNombre(sc.nextLine());
+        if (c != null) System.out.println(c.getNombre());
+    }
+
+    private static void dias(Scanner sc, Main repo) {
+        System.out.print(Constantes.DÍA + " ");
+        int dia = Integer.parseInt(sc.nextLine());
+        for (cumpleAgenda c : repo.buscarPorDia(dia)) {
+            System.out.println(c.getNombre());
+        }
+    }
+
+    private static void agenda(Main repo) {
+        for (cumpleAgenda c : repo.getLista()) {
+            System.out.println(c.getNombre());
+        }
+    }
+
+    private static void menu() {
+        System.out.println(Constantes.AGENDA);
+        System.out.println(Constantes.LISTAR);
+        System.out.println(Constantes.BUSCAR_POR_DÍA);
+        System.out.println(Constantes.BUSCAR_POR_NOMBRE);
+        System.out.println(Constantes.AÑADIR);
+        System.out.println(Constantes.ELIMINAR);
+        System.out.println(Constantes.SALIR);
     }
 }
